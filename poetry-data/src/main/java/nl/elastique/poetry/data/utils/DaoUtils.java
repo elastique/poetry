5374d36d05b32469f8051b7d5a397410d8f9137e
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2014-08-25 16:31:23 +0200

    created DaoUtils

---
+++ poetry-data/src/main/java/nl/elastique/poetry/data/utils/DaoUtils.java
@@ -0,0 +1,41 @@
+package nl.elastique.poetry.data.utils;
+
+import com.j256.ormlite.dao.Dao;
+
+public class DaoUtils
+{
+    public static void addColumn(Dao<?, ?> dao, String name, String type) throws java.sql.SQLException
+    {
+        String query = String.format("ALTER TABLE %s ADD COLUMN %s %s",
+                OrmliteUtils.getTableName(dao.getDataClass()),
+                name,
+                type);
+
+        dao.executeRawNoArgs(query);
+    }
+
+    public static void copyColumn(Dao<?, ?> dao, String fromName, String toName) throws java.sql.SQLException
+    {
+        String query = String.format("UPDATE %s SET %s = %s",
+                OrmliteUtils.getTableName(dao.getDataClass()),
+                toName,
+                fromName);
+
+        dao.executeRawNoArgs(query);
+    }
+
+    public static void createIndex(Dao<?, ?> dao, String columnName, String indexName) throws java.sql.SQLException
+    {
+        String query = String.format("CREATE INDEX %s ON %s (%s)",
+                indexName,
+                OrmliteUtils.getTableName(dao.getDataClass()),
+                columnName);
+
+        dao.executeRawNoArgs(query);
+    }
+
+    public static void createIndex(Dao<?, ?> dao, String columnName) throws java.sql.SQLException
+    {
+        createIndex(dao, columnName, String.format("%s_index", columnName));
+    }
+}
